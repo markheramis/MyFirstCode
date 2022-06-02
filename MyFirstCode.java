1956634a2304a10652967282fb5dea9b7e875ed7
Mark Clu Heramis <chumheramis@gmail.com>
2022-06-02 17:47:04 +0800

    initial commit
    Signed-off-by: Mark Clu Heramis <chumheramis@gmail.com>

---
+++ MyFirstCode.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author Mark
+ */
+public class MyFirstCode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
